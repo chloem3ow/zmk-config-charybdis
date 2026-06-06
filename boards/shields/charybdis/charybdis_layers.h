c5b64727f5971ea0c39da8d531371a73fd0e1030
Chloe Loveland <chloeloveland@proton.me>
2026-06-06 14:24:27 +0100

    change trackball sens

--- boards/shields/charybdis/charybdis_layers.h
+++ boards/shields/charybdis/charybdis_layers.h
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2024 The ZMK Contributors
  * SPDX-License-Identifier: MIT
- * 
+ *
  * Common layer definitions for Charybdis keyboard
  * Shared across all shield variants
  */
@@ -10,11 +10,10 @@
 
 // Layer definitions matching the keymap
 #define BASE 0
-#define POINTER 1
-#define LOWER 2
-#define RAISE 3
-#define SYMBOLS 4
-#define SCROLL 5
-#define SNIPING 6
-#define GAMING 7
-
+#define MEDIA 1
+#define NAV 2
+#define SYMBOLS 3
+#define NUMBERS 4
+#define FUNCTION 5
+#define MOUSE 6
+#define SCROLL 7
